8479480e133420a6ef7131d2d040a9ba389d96ab
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-29 16:33:12 -0700

    create CounterButton

---
+++ src/components/CounterButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CounterButton = () => {
+  return (
+    <button className="CounterButton">You clicked me X amount of times</button>
+  );
+};
+
+export default CounterButton;

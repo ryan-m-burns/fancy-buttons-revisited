26c09b934e683daacd7150290301c44062b02882
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-29 16:45:55 -0700

    add LightSwitchButton

---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LightSwitchButton = () => {
+  return (
+    <button className="LightSwitchButton">
+      {/* When the state is on */}
+      <span className="on">
+        <i>💡</i> I'm on!
+      </span>
+      {/* When the state is off */}
+      <span className="off">
+        <i>💡</i> I'm off!
+      </span>
+    </button>
+  );
+};
+
+export default LightSwitchButton;

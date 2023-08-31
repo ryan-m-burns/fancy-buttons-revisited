d8400406b9abc5bf4780d91604e9f94704cd364d
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-30 19:45:34 -0700

    import components into app and render buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import AngryButton from "./components/AngryButton";
+import CounterButton from "./components/CounterButton";
+import LightSwitchButton from "./components/LightSwitchButton";
+import TextRepeaterButton from "./components/TextRepeaterButton";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
+    </div>
+  );
 }
 
 export default App;

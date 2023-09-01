61d4c7dfbeeb10ceecb5d41a59a45f66ac6c8b16
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-31 22:32:41 -0700

    replace hardcode with state

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -3,11 +3,11 @@ import { useState } from "react";
 const TextRepeaterButton = (props) => {
   const [repetitions, setRepetitions] = useState();
 
-  return (
-    <button className="TextRepeaterButton">
-      <span>I like this text</span>
-    </button>
-  );
+  const textArray = [];
+  for (let i = 0; i < repetitions; i++) {
+    textArray.push(<span key={i}>I like this text</span>);
+  }
+  return <button className="TextRepeaterButton">{textArray}</button>;
 };
 
 export default TextRepeaterButton;

e616d2207544d1d567a3208959c06c925c02a3b0
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-09-04 15:16:21 -0700

    implement function to have LightSwitchButton turn on and off dark mode

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import CounterButton from "./components/CounterButton";
 import LightSwitchButton from "./components/LightSwitchButton";
 import TextRepeaterButton from "./components/TextRepeaterButton";
 import { useState } from "react";
+
 function App() {
   const [light, setLight] = useState("off");
   const [anger, setAnger] = useState(0);
   const [clickAmount, setClickAmount] = useState(0);
   const [repetitions, setRepetitions] = useState(1);
 
+  const dark = (light === 'off') ? 'dark' : '';
+
   return (
-    <div className={`App`}>
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton anger={anger} setAnger={setAnger} />

acfc2137dad542a7b4b4140ed8cbf7a973b4b59a
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-09-04 15:02:20 -0700

    clean up TextRepeaterButton

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -2,14 +2,19 @@ import { useState } from "react";
 
 const TextRepeaterButton = (props) => {
   const [repetitions, setRepetitions] = useState(1);
+  
   const textArray = [];
-
-  const handleClick = () => setRepetitions(repetitions + 1);
-
   for (let i = 0; i < repetitions; i++) {
     textArray.push(<span key={i}>I like this text </span>);
   }
-  return <button onClick={handleClick} className="TextRepeaterButton">{textArray}</button>;
+  
+  const handleClick = () => setRepetitions(repetitions + 1);
+
+  return (
+    <button onClick={handleClick} className="TextRepeaterButton">
+      {textArray}
+    </button>
+  );
 };
 
 export default TextRepeaterButton;

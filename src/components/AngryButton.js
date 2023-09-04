d9072c5c637c5469a588c4a906eb7bb2883b5d58
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-09-04 14:51:53 -0700

    add handleClick function for AngryButton

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -2,9 +2,18 @@ import { useState } from "react";
 
 const AngryButton = (props) => {
   const [anger, setAnger] = useState(0);
+
+  const handleClick = () => {
+    if (anger < 1) {
+      setAnger(anger + 0.1);
+    } else {
+      setAnger(0);
+    }
+  };
   return (
     <button
       style={{ backgroundColor: "rgba(255,0,0,${anger})" }}
+      onClick={handleClick}
       className="AngryButton"
     >
       {anger < 1 && <span>Don't click me too much!</span>}
